d11228def5faae5983558581ca9300373f57553b
QuentinDubois <quentin.dubois1@viacesi.fr>
2026-01-30 14:37:16 +0100

    Premier commit

---
+++ Workshop 3 genie logiciel/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EasySave
+{
+    class Program
+    {
+        static void Main()
+        {
+            string word;
+            Console.WriteLine("Entrez votre chaîne (1 à 8 caractères) : ");
+            word = Console.ReadLine();
+            Console.WriteLine("Vous avez choisi le mot : " + word); 
+            word = word.ToUpper();
+            Console.WriteLine();
+            Console.WriteLine("Voici votre mot en maj : "+ word);
+            Console.ReadKey();
+        }
+    }
+}
